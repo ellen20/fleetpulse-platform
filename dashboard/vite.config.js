0ca9fa6f7cec4e3a39ba2aeaa9d7575423bc20a0
Jingling Jin <jljin0524@gmail.com>
2026-05-01 10:30:23 -0500

    feat: add Docker and docker-compose for containerized local development

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://api:3001',
         changeOrigin: true
       }
     }
